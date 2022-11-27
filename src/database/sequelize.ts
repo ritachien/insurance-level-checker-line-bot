19f7c91802e9945c8b02de0531c6d3b6ee2a328e
Rita Chien <ritachien929@gmail.com>
2022-11-27 22:31:24 +0800

    feat: set configs for railway deploy

--- src/database/sequelize.ts
+++ src/database/sequelize.ts
@@ -1,11 +1,17 @@
 import { Sequelize } from "sequelize-typescript"
 import { LaborLevel } from "./models/LaborLevel"
+import * as dotenv from "dotenv"
+
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config()
+}
 
 export const sequelize = new Sequelize({
   dialect: 'mysql',
-  database: 'insurance_levels',
-  username: 'root',
-  password: 'password',
-  host: 'localhost',
+  database: process.env.MYSQLDATABASE,
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  host: process.env.MYSQLHOST,
+  port: Number(process.env.MYSQLPORT),
   models: [LaborLevel]
 })
