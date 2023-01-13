6653284ddf47367972027560b16c707be36327c8
Rita Chien <ritachien929@gmail.com>
2023-01-13 21:15:46 +0800

    feat: check sequelize cinnection with another function

--- src/server.ts
+++ src/server.ts
@@ -7,14 +7,14 @@ const port = process.env.PORT || 3000
 
 const start = async (): Promise<void> => {
   try {
-    await sequelize.sync()
-    logger.info('All models were synchronized successfully.')
+    await sequelize.authenticate()
+    logger.info('Connection has been established successfully.')
 
     createServer(app).listen(port, () => {
       logger.info(`Server running on port ${port}`)
     })
   } catch (error) {
-    logger.error(error)
+    logger.error('Unable to connect to the database:\n', error)
     process.exit(1)
   }
 }
